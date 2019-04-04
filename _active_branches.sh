f710e240095314e8ace30ab3c049061f9a45d1b2
Christophe Simonis <christophe@kn.gl>
2019-04-04 14:46:25 +0200

    [REL] welcome saas-12.3

--- _active_branches.sh
+++ _active_branches.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
-branches=(7.0 8.0 saas-6 9.0 10.0 saas-14 saas-15 11.0 saas-11.3 12.0 saas-12.1 saas-12.2 master)
+branches=(7.0 8.0 saas-6 9.0 10.0 saas-14 saas-15 11.0 saas-11.3 12.0 saas-12.1 saas-12.2 saas-12.3 master)
 declare -A prevs
 declare -A nexts
 for i in "${!branches[@]}"; do
